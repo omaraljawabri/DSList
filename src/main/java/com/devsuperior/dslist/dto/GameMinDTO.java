be9d507bb087e688736b1c8a57d6d191bdbb7c1e
Omar <164200540+omaraljawabri@users.noreply.github.com>
2024-11-11 16:14:52 -0300

    Game repository, service, controller, dto

---
+++ src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
@@ -0,0 +1,25 @@
+package com.devsuperior.dslist.dto;
+
+import com.devsuperior.dslist.entities.Game;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class GameMinDTO {
+    private Long id;
+    private String title;
+    private Integer year;
+    private String imgUrl;
+    private String shortDescription;
+
+    public GameMinDTO(Game game){
+        this.id = game.getId();
+        this.title = game.getTitle();
+        this.year = game.getYear();
+        this.imgUrl = game.getImgUrl();
+        this.shortDescription = game.getShortDescription();
+    }
+}
